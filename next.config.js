20ec82194ae8f35634894b03d0d7712f0aa503bc
milanPatel001 <mp28238@gmail.com>
2022-10-05 22:12:36 -0400

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["links.papareact.com", "image.tmdb.org"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
